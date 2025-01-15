e71c0c12e6f02812a670af09edd742518f9c8c6a
ifediniru <54271531+ifediniru@users.noreply.github.com>
2025-01-15 03:43:48 +0100

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -6,7 +6,7 @@ sonarqubeScanner({
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsAcadalearning',
-	    'sonar.login': 'sqa_b09f830ca25900b105dd457bb8f8d6d0d34d2493',
+	    'sonar.login': 'sqa_fc09e9cce86ed391b57dafffa25bab5d98e63122',
 	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
